0065c28ad0fcd8c089c688ce061b22eecf324a54
Mark Manders <mark.manders@ihomer.nl>
2014-06-02 20:16:21 +0200

    removed illegal import

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -1,25 +1,3 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import models.Fiddle;
-import org.codehaus.jackson.JsonNode;
-import org.junit.*;
-
-import play.mvc.*;
-import play.test.*;
-import play.data.DynamicForm;
-import play.data.validation.ValidationError;
-import play.data.validation.Constraints.RequiredValidator;
-import play.i18n.Lang;
-import play.libs.F;
-import play.libs.F.*;
-
-import static play.test.Helpers.*;
-import static org.fest.assertions.Assertions.*;
-
-
 /**
 *
 * Simple (JUnit) tests that can call all parts of a play app.
